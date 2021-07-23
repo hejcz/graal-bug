8a59bbf38b395e925b384d088fd020a9df1b6642
Julian Rubin <julian.rubin@rtbhouse.com>
2021-07-23 16:21:27 +0200

    add tests for big object

--- src/test/java/io/github/hejcz/GraalBugTest.java
+++ src/test/java/io/github/hejcz/GraalBugTest.java
@@ -1,27 +1,51 @@
 package io.github.hejcz;
 
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Engine;
 import org.graalvm.polyglot.Source;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.io.StringWriter;
 
 public class GraalBugTest {
 
     @Test
-    void shouldHandleRegex() throws IOException, InterruptedException {
-        Context context = Context.newBuilder().build();
-        String script = "(function abc() { 'some test text'.match('tes') })();";
-        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        try {
-            executorService.schedule(() -> context.close(true), 3, TimeUnit.SECONDS);
-            context.eval(Source.newBuilder("js", script, "test").build());
-        } finally {
-            executorService.shutdown();
-            executorService.awaitTermination(10, TimeUnit.SECONDS);
+    void passes() {
+        runWithMapWithXEntries(5_000);
+    }
+
+    @Test
+    void fails() {
+        // prepare script
+        runWithMapWithXEntries(10_000);
+    }
+
+    private void runWithMapWithXEntries(int entriesCount) {
+        // prepare script
+        StringWriter script = new StringWriter();
+        script.append("var map = {");
+        for (int i = 0; i < entriesCount; i++) {
+            String keyAndValue = String.format("%06d", i);
+            script.append("\"").append(keyAndValue).append("\": \"").append(keyAndValue).append("\", ");
+        }
+        script.append("};");
+        script.append("\n");
+        script.append("var x = 2 + 2;");
+
+        Context context = Context.newBuilder("js")
+                .engine(Engine.newBuilder().build())
+                .option("js.strict", "true")
+                .option("js.ecmascript-version", "12")
+                .build();
+
+        Source source = Source.create("js", script.toString());
+
+        // run script
+        for (int i = 0; i < 5_000; i++) {
+            if (i % 1_000 == 0) {
+                System.out.println(i);
+            }
+            context.eval(source);
         }
     }
 
