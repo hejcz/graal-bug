33070362239090be76f0a1e76f652f65196f3c82
Julian Rubin <julian.rubin@rtbhouse.com>
2021-07-27 12:38:57 +0200

    add test for context.close failing on OpenJDK16

---
+++ src/test/java/io/github/hejcz/SaveCloseTest.java
@@ -0,0 +1,36 @@
+package io.github.hejcz;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SaveCloseTest {
+
+    @Test
+    void shouldCloseContext() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("run: " + i);
+            Context context = Context.newBuilder()
+                    .build();
+            String script = "(function() { while(true); })";
+            ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+            try {
+                executorService.schedule(() -> context.close(true), 20, TimeUnit.MILLISECONDS);
+                Value fun = context.eval(Source.newBuilder("js", script, "test").build());
+                fun.execute();
+            } catch (Exception e) {
+                // ignore
+            } finally {
+                executorService.shutdown();
+                executorService.awaitTermination(10, TimeUnit.SECONDS);
+            }
+        }
+    }
+
+}
